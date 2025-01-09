69ba01504c1eb434f1a7e7538f2bf505660f0eff
Niklas Holl <niklas-holl@gmx.de>
2025-01-09 14:26:22 +0100

    Add synchronized clock implementation with periodic updates
    Introduced `SynchronizedClock` for time synchronization using ZeroMQ. Added methods for time synchronization, offset calculation, and periodic updates. Updated `Main` to demonstrate the synchronized clock functionality with example usage and output.

--- src/main/java/de/uulm/Main.java
+++ src/main/java/de/uulm/Main.java
@@ -1,7 +1,43 @@
 package de.uulm;
 
+import de.uulm.task1.SynchronizedClock;
+import org.zeromq.ZContext;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        startSynchronization(false);
+        startSynchronization(true);
     }
+
+    private static void startSynchronization(boolean useCurrentTimeToStart) {
+        try (ZContext context = new ZContext()) {
+            String serverHost = "tcp://vs.lxd-vs.uni-ulm.de:3322";
+
+            int numRequests = 5;
+            SynchronizedClock clock;
+            if (useCurrentTimeToStart) {
+                 clock = new SynchronizedClock(context, serverHost, numRequests, System.currentTimeMillis());
+            } else {
+                clock = new SynchronizedClock(context, serverHost, numRequests);
+                clock.startSynchronization();
+            }
+            System.out.println("Synchronisierte Zeit: " + time(clock.getTime()));
+            System.out.println("Aktueller Offset zur lokalen Zeit: " + clock.getOffset() + " ms");
+            Thread.sleep(5000);
+            System.out.println("Synchronisierte Zeit nach 5 Sekunden: " + time(clock.getTime()));
+            clock.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static final SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss");
+
+    public static String time(long time){
+        return format.format(new Date(time));
+    }
+
 }
